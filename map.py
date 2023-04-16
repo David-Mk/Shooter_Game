1363abbb2caaa4c6ab183f678267c09c40896c9e
David-Mk <mako.david00@gmail.com>
2023-04-17 00:09:12 +0300

    commit

---
+++ map.py
@@ -0,0 +1,44 @@
+import random
+import pygame as pg
+
+# Genrate arrays from 1 and _
+# array = [[random.choice([1, _]) for j in range(12)] for i in range(5)]
+# print(array)
+
+_ = False
+
+# One element corresponds to 100px 
+minimap = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, _, _, _, _, _, _, _, 1, _, _, _, _, _, _, 1],
+    [1, _, 1, _, _, _, 1, _, _, 1, _, 1, _, _, _, 1],
+    [1, _, _, 1, 1, _, 1, 1, _, _, _, _, 1, _, _, 1],
+    [1, _, _, 1, _, _, _, _, 1, 1, _, _, _, 1, _, 1],
+    [1, _, _, 1, _, 1, _, _, _, _, _, 1, _, _, _, 1],
+    [1, _, _, _, _, _, 1, _, _, 1, _, 1, _, 1, _, 1], 
+    [1, _, _, 1, _, _, _, _, _, _, _, _, _, _, _, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+]
+
+class Map:
+    
+    def __init__(self, game):
+        
+        self.game = game
+        self.minimap = minimap
+        self.worldmap = {}
+        
+        self.get_map()
+        
+    def get_map(self):
+        
+        for i, row in enumerate(self.minimap):
+            for j, value in enumerate(row):
+                
+                if value:
+                    self.worldmap[(j, i)] = value
+                    
+    def draw(self):
+          
+        [pg.draw.rect(self.game.screen, 'darkgray', (pos[0] * 100, pos[1] * 100, 100, 100), 2)
+        for pos in self.worldmap]
