ad330a0a1455647deb8090e843846a5959a3d244
David-Mk <mako.david00@gmail.com>
2023-04-20 23:20:41 +0300

    commit

--- raycasting.py
+++ raycasting.py
@@ -22,10 +22,28 @@ class RayCast:
             cos_a = math.cos(ray_angle)
             
             # horizontals
+            y_hor, dy = (y_map + 1, 1) if sin_a > 0 else (x_map - 1e-6, -1)
+            depth_hor = (y_map - oy) / sin_a
+            x_hor = ox + depth_hor * cos_a
+            
+            delta_depth = dy / sin_a
+            dx = delta_depth * cos_a
+            
+            for i in range(MAX_DEPTH):
+                
+                tile_hor = int(x_hor), int(y_hor)
+                
+                if tile_hor in self.game.world_map:
+
+                    break
+                
+                x_hor += dx
+                y_hot += dy
+                depth_hor += delta_depth
+            
             
             # verticals
             x_vert, dx = (x_map + 1, 1) if cos_a > 0 else (x_map - 1e-6, -1)
-            
             depth_vert = (x_vert - ox) / cos_a
             y_vert = oy + depth_vert * sin_a
             
